d485f201e31ca0fec30b7876138488ad0563c18f
yahyacan98 <sahanyahya@hotmail.com>
2022-04-04 01:53:41 +0300

    skill sistemi

---
+++ My project (4)/Assets/Scripts/Skills.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Skills : MonoBehaviour
+{
+
+    public int str=1;
+    public float agi=1;
+    public int sta=1;
+    public int skillpoints;
+    public int Exp;
+    public int PlayerLevel;
+    public int KickUpgrade;
+    public int PunchUpgrade;
+    public int PunchSkill;
+    public int KickSkill;
+    //public int currentStageLevel;
+    public int Gold;
+
+    public int BasepunchDamage = 10;
+    public int BasekickDamage = 20;
+
+    public int punchDamage;
+    public int kickDamage;
+
+    public float agiRatio;
+    public int extraHealth;
+    public float luckRatio;
+    public float strRatio;
+
+    //public SaveData saveData;
+
+
+    private void Awake()
+    {
+        //saveData = GameObject.Find("Main Camera").GetComponent<SaveData>();
+       
+             
+    }
+
+    private void Start()
+    {
+        CalculateStats();
+    }
+
+    public void CalculateStats()
+    {
+        strRatio = str + str / 7;
+        agiRatio = agi / 100;
+        punchDamage = BasepunchDamage + (int)strRatio;
+        kickDamage = BasekickDamage + (int)strRatio;
+        extraHealth = sta * 20;
+    }
+
+  
+
+   
+    
+
+    
+}
