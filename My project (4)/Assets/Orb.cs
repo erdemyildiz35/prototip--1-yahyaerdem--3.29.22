16aa8894529c172884cbf31c95e1bcfbc56bb4d8
yahyacan98 <sahanyahya@hotmail.com>
2022-05-17 23:54:47 +0300

    dessert map güncelleme
    dessert mapine orb sistemi eklendi ve düşmanlar eklendi

---
+++ My project (4)/Assets/Orb.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    public Canvas KeyEventCanvas;
+    public OrbsControl orbsControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbsControl = transform.parent.GetComponent<OrbsControl>();
+        KeyEventCanvas = transform.GetChild(0).gameObject.GetComponent<Canvas>();
+        KeyEventCanvas.enabled = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        KeyEventCanvas.enabled = true;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            orbsControl.DestroyedOrbs++;
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        KeyEventCanvas.enabled = false;
+    }
+}
